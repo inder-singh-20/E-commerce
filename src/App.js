41696619c725547db7f1e7897194e9f21eed0a19
Inder <crownis2010@gmail.com>
2022-07-27 17:44:26 +0530

    add login page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
+import Login from './Login'
 import {
   BrowserRouter as Router,
   Routes as Switch,
@@ -14,10 +15,10 @@ function App() {
   return (
    <Router>
      <div className="app">
-     <Header/>
       <Switch>
-      <Route path="/checkout" element={<Checkout/>}/>
-      <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route exact path="/checkout" element={<><Header/><Checkout/></>}/>
+      <Route exact path="/" element={<><Header/><Home/></>}/>
       </Switch>
     </div>
    </Router>
--- src/Header.js
+++ src/Header.js
@@ -31,12 +31,12 @@ function Header() {
 
          {/*Navbar options code starts*/}
          <div className="header__nav">
-        
+        <Link to="/login">
          <div className="header__option">
             <span className="header__optionLineOne">Hello Guest</span>
             <span className="header__optionLineTwo">Sign In</span>
           </div>
-        
+          </Link>
 
         
           <div className="header__option">
