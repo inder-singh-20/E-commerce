10214edc81f54a18fcc10aa8cc35742e600df0df
Inder <crownis2010@gmail.com>
2022-07-28 08:24:19 +0530

    deploy on firebase

--- src/Checkout.js
+++ src/Checkout.js
@@ -4,7 +4,7 @@ import Subtotal from './Subtotal'
 import CheckoutProduct from './CheckoutProduct'
 import {useStateValue} from './StateProvider'
 function Checkout() {
-    const [{ basket }, dispatch] = useStateValue();
+    const [{ basket,user}, dispatch] = useStateValue();
 
   return (
     <div className="checkout">
@@ -12,7 +12,9 @@ function Checkout() {
             <img className="checkout__ad"
             src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
             alt="ad"/>
+           
             <div>
+             <h3>Hello, {user?.email}</h3>
                 <h2 className="checkout__title">
                     Your Shopping Basket
                 </h2>
--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,16 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import firebase from 'firebase/compat/app';
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyD9VIyKGOX3NvRhrPpR4qqH4moVZL-t0CI",
-    authDomain: "clone-2b9ee.firebaseapp.com",
-    projectId: "clone-2b9ee",
-    storageBucket: "clone-2b9ee.appspot.com",
-    messagingSenderId: "931941776790",
-    appId: "1:931941776790:web:7d24177450e5628a8cf1b7",
-    measurementId: "G-5QSXEWXY6Y"
-  };
+  apiKey: "AIzaSyDdwco8xuOFKgnlwnqJioUPnE6fRm4675M",
+  authDomain: "clone1-1363a.firebaseapp.com",
+  projectId: "clone1-1363a",
+  storageBucket: "clone1-1363a.appspot.com",
+  messagingSenderId: "641436789627",
+  appId: "1:641436789627:web:ad6711ec5ae63ba9baabde",
+  measurementId: "G-SM83QGRD8E"
+};
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
 
